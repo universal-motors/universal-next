2ec56855544ff73cf66680d8895d7e6c115b8b98
mubashir-786 <mubashirrazaimran@gmail.com>
2023-10-30 18:03:40 +0500

    fix: header changes

--- components/ui/SearchBar.tsx
+++ components/ui/SearchBar.tsx
@@ -1,9 +1,6 @@
 "use client";
-import { IoCarSport, IoCarSportSharp } from "react-icons/io5";
-import { FcSearch } from "react-icons/fc";
 import { useRouter } from "next/navigation";
-import { FormEvent, useState, useTransition } from "react";
-import { TbTruck } from "react-icons/tb";
+import { FormEvent, useState } from "react";
 
 const vehicleTypes = [
   { id: "1", title: "Cars" },
@@ -15,6 +12,7 @@ export default function SearchBar() {
   const router = useRouter();
   const [searchCategory, setSearchCategory] = useState("1");
   const [searchKey, setSearchKey] = useState("");
+  const [dropdown, setDropdown] = useState(false);
 
   const onHandleSubmit = (event: FormEvent) => {
     event.preventDefault();
@@ -39,60 +37,118 @@ export default function SearchBar() {
 
     setSearchKey("");
   };
+  const getName = (val: string) => {
+    switch (val) {
+      case "1":
+        return "Cars";
 
-  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const result = event.target.id;
-    setSearchCategory(result);
+      case "2":
+        return "Trucks";
+
+      case "3":
+        return "Machinery";
+
+      default:
+        break;
+    }
   };
+
+  // const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const result = event.target.id;
+  //   setSearchCategory(result);
+  // };
 
   return (
     <div>
-      <form>
-        <div className="mt-2 flex rounded-md shadow-sm">
-          <div className="relative flex flex-grow items-stretch focus-within:z-10">
-            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-              <FcSearch className="h-5 w-5 text-gray-400" aria-hidden="true" />
-            </div>
-            <input
-              className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="Stock Code, Listing Title, Year of Listing, Model Code ..."
-              value={searchKey}
-              name="searchKey"
-              onChange={(e) => setSearchKey(e.target.value)}
-            />
-          </div>
-
+      <div className="flex mt-2">
+        <label
+          htmlFor="search-dropdown"
+          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        >
+          Your Email
+        </label>
+        <div className="relative">
           <button
+            onClick={() => {
+              setDropdown(!dropdown);
+            }}
+            id="dropdown-button"
+            data-dropdown-toggle="dropdown"
+            className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200   dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
             type="button"
-            onClick={onHandleSubmit}
-            className="transition duration-300 ease-in-out hover:scale-110 bg-yellow-400 relative -ml-px inline-flex items-center gap-x-1.5 rounded-r-md px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
           >
-            {/*{isPending && "Searching ... "}*/}
-            {/*{!isPending && <FcSearch className="-ml-0.5 h-5 w-5 text-gray-400" aria-hidden="true" />}*/}
-            Search
+            {getName(searchCategory)}
+            <svg
+              className="w-2.5 h-2.5 ml-2.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 4 4 4-4"
+              />
+            </svg>
+          </button>
+          <div
+            id="dropdown"
+            className={` ${
+              !dropdown && "hidden"
+            } absolute z-50  bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}
+          >
+            {vehicleTypes.map((item) => {
+              return (
+                <button
+                  onClick={() => {
+                    setSearchCategory(item.id);
+                  }}
+                  className="inline-flex  text-gray-700 text-sm w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  {item.title}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <div className="relative w-full">
+          <input
+            placeholder="Stock Code, Listing Title, Year of Listing, Model Code ..."
+            value={searchKey}
+            name="searchKey"
+            onChange={(e) => setSearchKey(e.target.value)}
+            type="search"
+            id="search-dropdown"
+            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300  dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+            required
+          />
+          <button
+            onClick={onHandleSubmit}
+            type="submit"
+            className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-[#FFB703] rounded-r-lg border border-blue-700 hover:bg-[#ffcf55]  dark:bg-[#ffb8038a] dark:hover:bg-[#ffb8038a] "
+          >
+            <svg
+              className="w-4 h-4"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+            <span className="sr-only">Search</span>
           </button>
         </div>
-        <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
-          {vehicleTypes.map((notificationMethod) => (
-            <div key={notificationMethod.id} className="flex items-center">
-              <input
-                id={notificationMethod.id}
-                name="notification-method"
-                type="radio"
-                defaultChecked={notificationMethod.id === "1"}
-                onChange={handleRadioChange}
-                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-              />
-              <label
-                htmlFor={notificationMethod.id}
-                className="ml-3 block text-sm font-medium leading-6 text-blue-100"
-              >
-                {notificationMethod.title}
-              </label>
-            </div>
-          ))}
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
