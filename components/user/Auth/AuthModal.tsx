eb1e43b7c8d7f9e7de50be6bd64867f04f4d53f6
razaahmad11 <razaqazhi@gmail.com>
2023-11-27 15:49:00 +0500

    fix: login

--- components/user/Auth/AuthModal.tsx
+++ components/user/Auth/AuthModal.tsx
@@ -225,7 +225,7 @@ export default function AuthModal({
               //   // cookiePolicy="single_host_origin"
               // />
               <button
-                className="text-sm  px-4 py-2 border flex justify-center items-center gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-200 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-400 dark:hover:text-slate-300 hover:shadow transition duration-150"
+                className="text-[10px] 2xl:text-sm px:1 py-1 2xl:px-4 2xl:py-2 border flex justify-center items-center gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-200 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-400 dark:hover:text-slate-300 hover:shadow transition duration-150"
               >
                 <img
                   className="w-6 h-6"
