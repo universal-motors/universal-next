4e053b6fec47338e43e34ab31bb6ed38be53ff39
razaahmad11 <razaqazhi@gmail.com>
2023-11-29 17:07:52 +0500

    fix:spacing

--- components/cars/CarSearchResult.tsx
+++ components/cars/CarSearchResult.tsx
@@ -332,7 +332,7 @@ export default function CarSearchResult({ locations, params }: Props) {
                     <h6 className="listname font-bold text-[12px] sm:text-[20px] lg:text-[12px] xl:text-[20px]  uppercase">
                       {car.listingTitle}
                     </h6>
-                    <h6 className="font-bold text-[12px] sm:text-[20px]  lg:text-[12px] xl:text-[20px] ">
+                    <h6 className="font-bold text-[12px] sm:text-[20px]  lg:text-[12px] xl:text-[20px] mt-0 2xl:!mt-2">
                       FOB Price:
                       <span className="mb-5 text-[12px] sm:text-[20px] lg:text-[12px] xl:text-[20px] ">
                         <PriceFormat carPrice={car.price} />
@@ -363,7 +363,7 @@ export default function CarSearchResult({ locations, params }: Props) {
                   {/*        </h6>*/}
                   {/*</div>*/}
                   <div className="askprice">
-                    <h6 className="text-[12px] sm:text-[20px] lg:text-[12px] xl:text-[20px]">
+                    <h6 className="text-[12px] sm:text-[20px] lg:text-[12px] xl:text-[20px] mt-0 2xl:!mt-2">
                       TOTAL Price:
                       <span>ASK</span>
                     </h6>
