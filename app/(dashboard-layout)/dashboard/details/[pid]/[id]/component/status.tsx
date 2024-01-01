1548fdf11afab167c9e98d57074de790381259fa
mubashir-786 <mubashirrazaimran@gmail.com>
2024-01-01 15:26:01 +0500

    fix: table issue resolved

--- app/(dashboard-layout)/dashboard/details/[pid]/[id]/component/status.tsx
+++ app/(dashboard-layout)/dashboard/details/[pid]/[id]/component/status.tsx
@@ -8,42 +8,46 @@ type Prop = {
 export default function Status({ stock }: Prop) {
     return (
         <div className="row w-[95%] m-auto">
-            <div className="w-full py-2  bg-[#221C63] mt-7 mb-3">
-                <p className="text-white font-semibold">Paid / Remaining Amount</p>
+            <div className="w-full py-2 flex   bg-[#221C63] mt-7 mb-3">
+                <div className="w-1/2">
+                    <p className="text-white font-semibold ">Paid / Remaining Amount</p>
+                </div>
+                <div className="w-1/2">
+                    <p className="text-white font-semibold ">STATUS</p>
+
+                </div>
+
             </div>
-            <div className="w-full ">
-                <div className="ml-6">
-                    <div className="flex gap-3 items-center">
-                        <div className="w-4 h-4 bg-[#221C63]"></div>
-                        <p>Paid {stock?.paidPercent}% </p>
+            <div className="w-full flex flex-wrap">
+                <div className="w-1/2 ">
+                    <div className="ml-6">
+                        <div className="flex gap-3 items-center">
+                            <div className="w-4 h-4 bg-[#221C63]"></div>
+                            <p>Paid {stock?.paidPercent}% </p>
+                        </div>
+                        <div className="flex gap-3  items-center">
+                            <div className="w-4 h-4 bg-[#9ca3af]"></div>
+                            Remaining {stock?.balancePercent}%
+                        </div>
                     </div>
-                    <div className="flex gap-3  items-center">
-                        <div className="w-4 h-4 bg-[#9ca3af]"></div>
-                        Remaining {stock?.balancePercent}%
+                    <div className="w-[300px] mt-2 mx-auto">
+                        <PieChart
+                            animate
+                            data={[
+                                { title: "Paid", value: stock?.paidPercent ?? 0, color: "#221C63" },
+                                {
+                                    title: "unpaid",
+                                    value: stock?.balancePercent ?? 0,
+                                    color: "#9ca3af",
+                                },
+                            ]}
+                        />
+                    </div>
+                    <div className="flex gap-3  font-bold items-center">
+                        Total Price:  <PriceFormat carPrice={stock?.totalCnf ?? 0} />
                     </div>
                 </div>
-                <div className="w-[300px] mt-2 mx-auto">
-                    <PieChart
-                        animate
-                        data={[
-                            { title: "Paid", value: stock?.paidPercent ?? 0, color: "#221C63" },
-                            {
-                                title: "unpaid",
-                                value: stock?.balancePercent ?? 0,
-                                color: "#9ca3af",
-                            },
-                        ]}
-                    />
-                </div>
-                <div className="flex gap-3  font-bold items-center">
-                    Total Price:  <PriceFormat carPrice={stock?.totalCnf ?? 0} />
-                </div>
-            </div>
-            <div className="w-full py-2  bg-[#221C63] mt-3">
-                <p className="text-white font-semibold">STATUS</p>
-            </div>
-            <div className="row features-table !w-full mt-3">
-                <table className="keyinfo !w-full">
+                <table className="keyinfo !w-1/2">
                     <thead>
                         <tr>
                             {/* <td className="first !text-[12px] sm:!text-[16px]">Stage</td> */}
@@ -53,16 +57,7 @@ export default function Status({ stock }: Prop) {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
 
-                            <td className=" !text-[12px] sm:!text-[16px]">
-                                Documents Dispatched
-                            </td>
-
-                            <td className="!text-[12px] sm:!text-[16px]">
-                                {stock?.release ? "✔️" : "❌"}
-                            </td>
-                        </tr>
                         <tr>
                             <td className=" !text-[12px] sm:!text-[16px]">
                                 OK to Book
@@ -93,6 +88,16 @@ export default function Status({ stock }: Prop) {
                             </td>
                             <td className="!text-[12px] sm:!text-[16px]">
                                 {stock?.courierPersonName ? "✔️" : "❌"}
+                            </td>
+                        </tr>
+                        <tr>
+
+                            <td className=" !text-[12px] sm:!text-[16px]">
+                                Documents Dispatched
+                            </td>
+
+                            <td className="!text-[12px] sm:!text-[16px]">
+                                {stock?.release ? "✔️" : "❌"}
                             </td>
                         </tr>
                     </tbody>
